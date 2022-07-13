0cb89199907f4e959fb00337e69763d66408b594
Jon Matthews <joncarlmatthews@gmail.com>
2022-07-13 10:41:51 +0100

    Small changes after working through local set-up

--- index.js
+++ index.js
@@ -1,68 +1,114 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
-const {google} = require('googleapis');
-const { sheets } = require('googleapis/build/src/apis/sheets');
-const sendQuery = require('./utils/sendQuery');
-const spreadsheetId = "1KioIZQsiAHmgvc0_7fxDfhvT1a0vnp6Vzez4eEhbtEg";
+const express = require("express");
+const { google } = require("googleapis");
+const sendQuery = require("./utils/sendQuery");
 
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+const spreadsheetId = process.env.GOOGLE_SHEET_ID;
 
 const app = express();
 app.use(express.json());
 
+/**
+ * Google Sheet connector test endpoint that outputs the metadata related to the
+ * sheet
+ */
+app.get("/", async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
 
-app.get('/', async (req, res) => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets"
-    });
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
-    const client = await auth.getClient();
-    const googleSheets = google.sheets({version: "v4", auth: client});
-
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId
-    });
-    res.send(metaData.data);
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+  res.send(metaData.data);
 });
 
+/**
+ * Shopify connector test endpoint that outputs the "myshopify" domain
+ */
 app.get("/testG", async (req, res) => {
-    const query = `{
+  const query = `{
         shop{
           myshopifyDomain
         }
-      }`
+      }`;
 
-      try{
-      const response = await sendQuery(query);
-      res.send(response);
-      }catch(error){
-        console.log(error);
-        res.send('Error');
-      }
-
+  try {
+    const response = await sendQuery(query);
+    res.send(response);
+  } catch (error) {
+    console.log(error.message);
+    res.send("Error");
+  }
 });
 
+/**
+ * Handles the "Order creation" webhook notification from Shopify.
+ *
+ * Takes the order payload and writes it into the Orders tab within the Google
+ * sheet
+ */
+app.post("/newOrder", async (req, res) => {
+  const { email, total_price, total_tax, created_at, name } = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Orders!A:E",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[name, created_at, email, total_price, total_tax]],
+    },
+  });
+
+  res.sendStatus(200);
+});
+
+/**
+ * Product update endpoint that takes the data from the Google Sheet and pushes
+ * it back to Shopify.
+ */
 app.post("/productUpdate", async (req, res) => {
+  const id = req.body.id;
+  const price = req.body.price + "";
+  const newInventoryLevel = req.body.inventory;
 
-    const id = req.body.id;
-    const price = req.body.price+"";
-    const newInventoryLevel = req.body.inventory;
-    
-    const updatePriceCall = await updatePrice(id, price);
-    if (updatePriceCall.statusCode==200){
-        const oldInventoryLevel = JSON.parse(updatePriceCall.body).productVariantUpdate.productVariant.inventoryItem.inventoryLevel.available;
-        const inventoryLevelId = JSON.parse(updatePriceCall.body).productVariantUpdate.productVariant.inventoryItem.inventoryLevel.id;
+  const updatePriceCall = await updatePrice(id, price);
+  if (updatePriceCall.statusCode == 200) {
+    const oldInventoryLevel = JSON.parse(updatePriceCall.body)
+      .productVariantUpdate.productVariant.inventoryItem.inventoryLevel
+      .available;
+    const inventoryLevelId = JSON.parse(updatePriceCall.body)
+      .productVariantUpdate.productVariant.inventoryItem.inventoryLevel.id;
 
-        if (newInventoryLevel != oldInventoryLevel){
-            await updateInventoryLevel(inventoryLevelId, newInventoryLevel-oldInventoryLevel);
-        }
+    if (newInventoryLevel != oldInventoryLevel) {
+      await updateInventoryLevel(
+        inventoryLevelId,
+        newInventoryLevel - oldInventoryLevel
+      );
     }
-    res.sendStatus(200);
+  }
+
+  res.sendStatus(200);
 });
 
-async function updatePrice(id, price){
-    const query = `mutation productVariantUpdate($priceInput: ProductVariantInput!) {
+async function updatePrice(id, price) {
+  const query = `mutation productVariantUpdate($priceInput: ProductVariantInput!) {
         productVariantUpdate(input: $priceInput) {
           productVariant {
             id,
@@ -80,32 +126,32 @@ async function updatePrice(id, price){
             message
           }
         }
-      }`
+      }`;
 
-      const variables = {
-        "priceInput":{
-            "id": id,
-            "price": price
-        }
-      }
+  const variables = {
+    priceInput: {
+      id: id,
+      price: price,
+    },
+  };
 
-      try{
-        const res = await sendQuery(query, variables);
-        return {
-            statusCode: 200,
-            body: JSON.stringify(res)
-        }
-      }catch(error){
-        console.log(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify(error)
-        }
-      }
+  try {
+    const res = await sendQuery(query, variables);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
 }
 
-async function updateInventoryLevel(id, delta){
-    const query = `mutation inventoryAdjustQuantity($input: InventoryAdjustQuantityInput!) {
+async function updateInventoryLevel(id, delta) {
+  const query = `mutation inventoryAdjustQuantity($input: InventoryAdjustQuantityInput!) {
         inventoryAdjustQuantity(input: $input) {
           inventoryLevel {
             available
@@ -115,66 +161,30 @@ async function updateInventoryLevel(id, delta){
             message
           }
         }
-      }`
+      }`;
 
-      const variables = {
-      "input": {
-        "availableDelta": delta,
-        "inventoryLevelId": id
-      }
-    }
+  const variables = {
+    input: {
+      availableDelta: delta,
+      inventoryLevelId: id,
+    },
+  };
 
-    try {
-        const res = await sendQuery(query, variables);
-        return {
-            statusCode: 200,
-            body: JSON.stringify(res)
-        }
-    }catch(error){
-        console.log(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify(error)
-        }
-    }
+  try {
+    const res = await sendQuery(query, variables);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
 }
 
-
-
-
-app.post('/newOrder', async (req, res) => {
-    
-    const {email, total_price, total_tax, created_at, name} = req.body;
-
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets"
-    });
-
-    const client = await auth.getClient();
-    const googleSheets = google.sheets({version: "v4", auth: client});
-
-    await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Orders!A:E",
-        valueInputOption: "USER_ENTERED",
-        resource: {
-            values: [[name, created_at, email, total_price, total_tax]],
-        }
-    });
-
-    res.sendStatus(200);
-});
-
-
-
-
-
-
-
-
-
 app.listen(port, (req, res) => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
--- utils/sendQuery.js
+++ utils/sendQuery.js
@@ -1,27 +1,27 @@
-require('dotenv').config()
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 
-const url = `https://${process.env.API_KEY}:${process.env.PW}@${process.env.SHOP_NAME}.myshopify.com/admin/api/2022-07/graphql.json`
-
+const url = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_TOKEN}@${process.env.SHOPIFY_SHOP_NAME}.myshopify.com/admin/api/${process.env.SHOPIFY_API_VER}/graphql.json`;
 
 module.exports = async (query, variables) => {
-    const {data:{data, errors}} = await axios({
-        url: url,
-        method: "POST",
-        headers:{
-            Accept: "application/json"
-        },
-        data:{
-            query,
-            variables
-        }
-    });
+  const {
+    data: { data, errors },
+  } = await axios({
+    url: url,
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+    data: {
+      query,
+      variables,
+    },
+  });
 
-    if (errors){
-        console.error(errors);
-        throw new Error("Error accessing graphql");
-    }
+  if (errors) {
+    console.error(errors);
+    throw new Error("Error accessing graphql");
+  }
 
-    return data;
-
-}
+  return data;
+};
